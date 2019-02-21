7ae03f568a4ce532332cad7eda4792d77f26b495
felipeaugustox <augustto.felipe@hotmail.com>
2019-02-21 14:49:16 -0300

    Update bot

--- cron/cron_daily_tio_patinhas.py
+++
@@ -1,8 +0,0 @@
-from crontab import CronTab
-
-cron = CronTab(user=True)  
-job = cron.new(command='/Users/felipe/projects/daily-bot/script/daily.py')
-job.setall('* * * * *')
-job.enable()
-
-cron.write()    
